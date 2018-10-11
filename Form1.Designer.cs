4ea4fbb9f795923faaac7280d75917d245497c76
Tatsu68 <Tatsu68@users.noreply.github.com>
2018-10-12 01:28:42 +0800

    Bug Fixes

---
+++ Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.numericUpDownW = new System.Windows.Forms.NumericUpDown();
+            this.labelW = new System.Windows.Forms.Label();
+            this.labelH = new System.Windows.Forms.Label();
+            this.numericUpDownH = new System.Windows.Forms.NumericUpDown();
+            this.buttonBeginTransfer = new System.Windows.Forms.Button();
+            this.textBoxOpen = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxSave = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownW)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownH)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // numericUpDownW
+            // 
+            this.numericUpDownW.Location = new System.Drawing.Point(275, 149);
+            this.numericUpDownW.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.numericUpDownW.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownW.Name = "numericUpDownW";
+            this.numericUpDownW.Size = new System.Drawing.Size(84, 21);
+            this.numericUpDownW.TabIndex = 1;
+            this.numericUpDownW.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // labelW
+            // 
+            this.labelW.AutoSize = true;
+            this.labelW.Location = new System.Drawing.Point(218, 151);
+            this.labelW.Name = "labelW";
+            this.labelW.Size = new System.Drawing.Size(53, 12);
+            this.labelW.TabIndex = 2;
+            this.labelW.Text = "宽（1-）";
+            // 
+            // labelH
+            // 
+            this.labelH.AutoSize = true;
+            this.labelH.Location = new System.Drawing.Point(399, 151);
+            this.labelH.Name = "labelH";
+            this.labelH.Size = new System.Drawing.Size(71, 12);
+            this.labelH.TabIndex = 3;
+            this.labelH.Text = "高（1-120）";
+            // 
+            // numericUpDownH
+            // 
+            this.numericUpDownH.Location = new System.Drawing.Point(476, 149);
+            this.numericUpDownH.Maximum = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.numericUpDownH.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownH.Name = "numericUpDownH";
+            this.numericUpDownH.Size = new System.Drawing.Size(84, 21);
+            this.numericUpDownH.TabIndex = 4;
+            this.numericUpDownH.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // buttonBeginTransfer
+            // 
+            this.buttonBeginTransfer.Location = new System.Drawing.Point(290, 176);
+            this.buttonBeginTransfer.Name = "buttonBeginTransfer";
+            this.buttonBeginTransfer.Size = new System.Drawing.Size(218, 40);
+            this.buttonBeginTransfer.TabIndex = 5;
+            this.buttonBeginTransfer.Text = "开始转换成fsc（你也事音le人辣！）";
+            this.buttonBeginTransfer.UseVisualStyleBackColor = true;
+            this.buttonBeginTransfer.Click += new System.EventHandler(this.buttonBeginTransfer_Click);
+            // 
+            // textBoxOpen
+            // 
+            this.textBoxOpen.Location = new System.Drawing.Point(220, 27);
+            this.textBoxOpen.Name = "textBoxOpen";
+            this.textBoxOpen.ReadOnly = true;
+            this.textBoxOpen.Size = new System.Drawing.Size(315, 21);
+            this.textBoxOpen.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(218, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "图片文件";
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(549, 20);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(51, 32);
+            this.buttonOpen.TabIndex = 8;
+            this.buttonOpen.Text = "选择";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 227);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(588, 138);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = resources.GetString("richTextBox1.Text");
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(218, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "fsc目录";
+            // 
+            // textBoxSave
+            // 
+            this.textBoxSave.Location = new System.Drawing.Point(220, 88);
+            this.textBoxSave.Name = "textBoxSave";
+            this.textBoxSave.ReadOnly = true;
+            this.textBoxSave.Size = new System.Drawing.Size(315, 21);
+            this.textBoxSave.TabIndex = 11;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(549, 81);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(51, 32);
+            this.buttonSave.TabIndex = 12;
+            this.buttonSave.Text = "选择";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(19, 378);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(580, 85);
+            this.textBox3.TabIndex = 13;
+            this.textBox3.Text = "YOUR MIDI IS SHIT";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "模式";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "单色",
+            "16色"});
+            this.comboBox1.Location = new System.Drawing.Point(253, 119);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 15;
+            this.comboBox1.Text = "单色";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 489);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxOpen);
+            this.Controls.Add(this.buttonBeginTransfer);
+            this.Controls.Add(this.numericUpDownH);
+            this.Controls.Add(this.labelH);
+            this.Controls.Add(this.labelW);
+            this.Controls.Add(this.numericUpDownW);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "彩色粪midi生成器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownW)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownH)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDownW;
+        private System.Windows.Forms.Label labelW;
+        private System.Windows.Forms.Label labelH;
+        private System.Windows.Forms.NumericUpDown numericUpDownH;
+        private System.Windows.Forms.Button buttonBeginTransfer;
+        private System.Windows.Forms.TextBox textBoxOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxSave;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,335 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        string pathOpen, pathSave;
+        bool openFlag = false, saveFlag = false;
+        Image imOriginal;
+        Bitmap bmOriginal;
+        Graphics g;
+        Bitmap bmBuffer;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBeginTransfer_Click(object sender, EventArgs e)
+        {
+            if (!openFlag || !saveFlag)
+            {
+                MessageBox.Show("恁这池沼，先选取好路径可否？", "我谔谔", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+
+
+            BinaryWriter bw = new BinaryWriter(new FileStream(pathSave, FileMode.Create, FileAccess.Write, FileShare.ReadWrite));
+
+            try
+            {
+                bw.Write(0x64684C46);
+                bw.Write(0x00000006);
+                bw.Write(0x00010010);
+                bw.Write(0x4C460060);
+                bw.Write(0x00117464);
+                bw.Write(0x0AC70000);
+                bw.Write(0x352E3231);
+                bw.Write(0x352E302E);
+                bw.Write(0x031C0038);
+                bw.Write((short)0x0041);
+                bw.Write((byte)0x00);
+                bw.Write((byte)0xE0);
+
+                //test use
+                int xm = Convert.ToInt32(numericUpDownW.Value);
+                int ym = Convert.ToInt32(numericUpDownH.Value);
+                int sizeM = xm * ym;
+                //MessageBox.Show(sizeM.ToString());
+                // check how much should checking number be
+                int chk = 0;
+                int tt = 256;
+                int chkCount = 1;
+                for (int i = 0; i < sizeM; i++)
+                {
+                    chk += 24;
+                    int tBorder = 1;
+                    for (int j = 1; j <= chkCount; j++)
+                    { 
+                        tBorder *= 256;
+                        if (j < chkCount)
+                        {
+                            if (chk % tBorder < (tBorder / 2))
+                            {
+                                chk += tBorder / 2;
+                            }
+                        }
+                    }
+                    if (chk >= tBorder / 2)
+                    {
+                        chkCount += 1;
+                        chk += tBorder;
+                    }
+                }
+                //MessageBox.Show(chk.ToString());
+                for (int i = 0; i < chkCount; i++)
+                {
+                    bw.Write((byte)(chk % 256));
+                    chk /= 256;
+                }
+
+                //Do Color Picking
+                bmBuffer = new Bitmap(xm, ym);
+                g = Graphics.FromImage(bmBuffer);
+                g.DrawImage(bmOriginal, 0,0, bmBuffer.Width,bmBuffer.Height);
+
+                //Draw
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    for (int i = 0; i < xm; i++)
+                    {
+                        for (int j = 0; j < ym; j++)
+                        {
+                            Color pixel = bmBuffer.GetPixel(i, j);
+                            //if (i == 10 && j == 10) MessageBox.Show(pixel.ToString());
+                            byte vel = (byte)((pixel.R + pixel.G + pixel.B) / 3 * 128 / 255);
+                            WritePixelNote(bw, 0, vel, i * 24, 120 - j);
+                        }
+                    }
+                }
+                else
+                {
+
+                    for (int i = 0; i < xm; i++)
+                    {
+                        for (int j = 0; j < ym; j++)
+                        {
+                            Color pixel = bmBuffer.GetPixel(i, j);
+                            //if (i == 10 && j == 10) MessageBox.Show(pixel.ToString());
+                            byte channel = 0;
+                            byte vel = (byte)((pixel.R + pixel.G + pixel.B) / 3 * 128 / 255);
+                            RGBData rgbd = new RGBData(pixel.R, pixel.G, pixel.B);
+                            HSVData hsvd = getHSV(rgbd);
+
+                            int hc = hsvd.h;
+                            if (hc < 85) hc = hc - 85 + 256;
+                            else hc -= 85;
+                            channel = (byte)(hc / 16);
+
+
+                            WritePixelNote(bw, channel, vel, i * 24, 120 - j);
+
+
+                        }
+                    }
+
+
+                    /*
+                    bw.Close();
+                    bw.Dispose();
+                    bw = null;
+                    MessageBox.Show("16色正在开发中，创歉，，，", "我谔谔", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;*/
+                }
+
+
+            }
+            catch (Exception)
+            {
+                bw.Close();
+                bw.Dispose();
+                bw = null;
+                MessageBox.Show("哎呀，出了点问题，创歉，，，", "我谔谔", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            bw.Close();
+            bw.Dispose();
+            bw = null;
+            MessageBox.Show("你的大粪完成了！快导入FL姐贵的小窗窗然后去批站投稿罢！", "好时代，来临了！", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return;
+
+
+        }
+
+        public void WritePixelNote(BinaryWriter bw, byte channel, byte vel, int x, int y)
+        {
+            int xt = x;
+            int xb = 0;
+            /*for (int i = 0; i < 8; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    xb *= 256;
+                    xb += xt % 16;
+                }
+                else
+                {
+                    xb += (xt % 16) * 16;
+                }
+                xt = xt / 16;
+            }*/
+            bw.Write(xt); //4
+            bw.Write(0x00004000); //4
+            bw.Write(0x00000018); //4
+            bw.Write((byte)y); //1
+            bw.Write(0x78000000); //4
+            bw.Write((short)0x4000); //2
+            bw.Write(channel); //1
+            bw.Write((byte)0x40); //1
+            bw.Write(vel); // 1
+            bw.Write((byte)0x80);
+            bw.Write((byte)0x80);
+        }
+
+
+
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = false;
+            ofd.Filter = "All files(*.*)|*.*";
+            try
+            {
+
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pathOpen = ofd.FileName;
+                    imOriginal = Image.FromFile(pathOpen);
+                    pictureBox1.Image = imOriginal;
+                    if(bmOriginal!= null)
+                    {
+                        bmOriginal.Dispose();
+                        bmOriginal = null;
+                    }
+                    bmOriginal = new Bitmap(imOriginal);
+                    textBoxOpen.Text = pathOpen;
+                    openFlag = true;
+                    //MessageBox.Show(bmOriginal.GetPixel(30, 30).ToString());
+                }
+            }
+            catch
+            {
+                MessageBox.Show("恁到底知道什么叫图片文件🐎？", "我谔谔", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                ofd.Dispose();
+                ofd = null;
+                return;
+            }
+
+
+            ofd.Dispose();
+            ofd = null;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "FL Studio Score(*.fsc)|*.fsc";
+            
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                pathSave = sfd.FileName;
+                textBoxSave.Text = pathSave;
+                saveFlag = true;
+            }
+            sfd.Dispose();
+            sfd = null;
+        }
+
+
+
+        public HSVData getHSV(RGBData rgb)
+        {
+            byte h, s, v;
+            float hf, sf, vf;
+            float rf = (float)rgb.r / 255;
+            float gf = (float)rgb.g / 255;
+            float bf = (float)rgb.b / 255;
+            float max, min;
+            max = Math.Max(rf, gf);
+            max = Math.Max(max, bf);
+            min = Math.Min(rf, gf);
+            min = Math.Min(min, bf);
+            float d = max - min;
+
+            if(d==0)
+            {
+                hf = 0;
+            }
+            else if(max==rf)
+            {
+                hf = ((gf-bf)/d)/ 6;
+            }
+            else if (max == gf)
+            {
+                hf= ((bf - rf) / d + 2) / 6;
+            }
+            else
+            {
+                hf =((rf - gf) / d + 4) / 6;
+            }
+
+            if (max == 0) sf = 0;
+            else sf = d / max;
+
+            vf = max;
+
+            h = (byte)(hf * 255);
+            s = (byte)(sf * 255);
+            v = (byte)(vf * 255);
+
+            return new HSVData(h,s,v);
+
+        }
+
+
+
+    }
+
+
+    public class RGBData
+    {
+        public byte r, g, b;
+        public RGBData(byte r, byte g, byte b)
+        {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+    }
+
+    public class HSVData
+    {
+        public byte h, s, v;
+        public HSVData(byte h, byte s, byte v)
+        {
+            this.h = h;
+            this.s = s;
+            this.v = v;
+        }
+
+    }
+}
